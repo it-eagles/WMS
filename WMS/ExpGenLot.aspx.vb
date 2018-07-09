f929cd33573f9f137f0768316953d54e99948b8c
EAGLESGROUP\nattaphon <nattaphon.k@eaglesthai.com>
2018-07-09 17:36:02 +0700

    เพิ่มform

---
+++ WMS/ExpGenLot.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class ExpGenLot
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class
