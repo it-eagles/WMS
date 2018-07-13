f00895e84dea8629ae8dc011fa6655590b26bafc
EAGLESGROUP\ittn <bosspakin@gmail.com>
2018-07-13 17:36:41 +0700

    -Add FinishGoodWH.aspx      Completed -Add ChkStatMoveWH.aspx   Completed -Add ChkStockWH.aspx         Completed -Add AdLocSCRAPWH.aspx  Completed -Add AdLocWH.aspx               Not Done

---
+++ WMS/AdLocWH.aspx.vb
@@ -0,0 +1,15 @@
+﻿Public Class AdLocWH
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnFind_ServerClick(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnSave_ServerClick(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
---
+++ WMS/FinishGoodWH.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class FinishGoodWH
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnSumQTY_ConGoodRec_ServerClick(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
