63f66f4b334c89ec412dabf4c7d5b21d94f612d3
Pakin Ch <bosspakin@gmail.com>
2018-09-25 14:20:47 +0700

    no message

---
+++ WMS/RptStorageAgingFrm.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class RptStorageAgingFrm
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnReport_ServerClick(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
