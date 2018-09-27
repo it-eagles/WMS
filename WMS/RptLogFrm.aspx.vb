80eb120c6208610e0c00cce37c784f44c6b11eeb
Pakin Ch <bosspakin@gmail.com>
2018-09-27 11:28:38 +0700

    ไปให้สุดแล้วหยุดที่ทำใหม่

--- WMS/RptLogFrm.aspx.vb
+++ WMS/RptLogFrm.aspx.vb
@@ -1,19 +1,97 @@
-<<<<<<< HEAD
-Option Explicit On
-Option Strict On
-Option Infer On
-
-Public Class RptLogfrm
-=======
-Public Class RptlogFrm
->>>>>>> 8f9c1eba2d5077fd8b5eb134726eb9b44630a7ed
+﻿Public Class RptLogFrm
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        txtdatepickerDate.Text = Now
+        txtdatepickerToDate.Text = Now
     End Sub
 
     Protected Sub btnPrint_ServerClick(sender As Object, e As EventArgs)
+        Dim CheckLog As String = ddlSelectImEx.Text.Trim
+        Dim CheckBy As String = ""
+        Dim InvoiceNo As String = txtInvoiceNo.Value.Trim
+        Dim _cultureEnInfo As New Globalization.CultureInfo("en-US")
+        Dim formdate As String = CStr(Convert.ToDateTime(Me.txtdatepickerDate.Text, _cultureEnInfo).ToString("dd/MM/yyyy"))
+        Dim toDate As String = CStr(Convert.ToDateTime(Me.txtdatepickerToDate.Text, _cultureEnInfo).ToString("dd/MM/yyyy"))
+
+        If ddlSelectImEx.Text.Trim = "LogInvoice" Or ddlSelectImEx.Text.Trim = "LogInvoiceDetail" Or ddlSelectImEx.Text.Trim = "LogPackingList" _
+            Or ddlSelectImEx.Text.Trim = "LogImpInvoice" Or ddlSelectImEx.Text.Trim = "LogImpInvoiceDetail" Or ddlSelectImEx.Text.Trim = "LogImpPackingList" Then
+            If rdbByDate.Checked = True Then
+                If String.IsNullOrEmpty(txtdatepickerDate.Text.Trim) Then
+                    ScriptManager.RegisterStartupScript(Me, Me.GetType(), "alertMessage", "alert('กรุณาใส่ Date Start ก่อน');", True)
+                    Exit Sub
+                End If
+                If String.IsNullOrEmpty(txtdatepickerToDate.Text.Trim) Then
+                    ScriptManager.RegisterStartupScript(Me, Me.GetType(), "alertMessage", "alert('กรุณาใส่ Date From ก่อน');", True)
+                    Exit Sub
+                End If
+
+                CheckBy = "ByDate"
+
+                Session("CheckLog") = CheckLog
+                Session("CheckBy") = CheckBy
+                Session("formdate") = formdate
+                Session("toDate") = toDate
+                'Session("InvoiceNo") = InvoiceNo
+
+                Dim url As String = "ShowReport/ShowRptLog.aspx?Fromdate=" + formdate + "&Todate=" + toDate
+                ScriptManager.RegisterStartupScript(Me, Me.GetType(), "onclick", "javascript:window.open( '" + url + "','_blank','height=600px,width=1000px,scrollbars=1');", True)
+
+            ElseIf rdbByInvoice.Checked = True Then
+                If txtInvoiceNo.Value.Trim = "" Then
+                    ScriptManager.RegisterStartupScript(Me, Me.GetType(), "alertMessage", "alert('กรุณาใส่ Invoice No ก่อน');", True)
+                    Exit Sub
+                End If
+                CheckBy = "ByInv"
+
+                Session("CheckLog") = CheckLog
+                Session("CheckBy") = CheckBy
+                'Session("formdate") = formdate
+                'Session("toDate") = toDate
+                Session("InvoiceNo") = InvoiceNo
+
+                Dim url As String = "ShowReport/ShowRptLog.aspx?InvNo=" + InvoiceNo
+                ScriptManager.RegisterStartupScript(Me, Me.GetType(), "onclick", "javascript:window.open( '" + url + "','_blank','height=600px,width=1000px,scrollbars=1');", True)
+            End If
+
+        ElseIf ddlSelectImEx.Text.Trim = "LogBrand" Or ddlSelectImEx.Text.Trim = "LogCarLicense" Or ddlSelectImEx.Text.Trim = "LogCountry" Or ddlSelectImEx.Text.Trim = "LogConsignnee" _
+            Or ddlSelectImEx.Text.Trim = "LogCurrency" Or ddlSelectImEx.Text.Trim = "LogDeliveryTerm" Or ddlSelectImEx.Text.Trim = "LogDriver" Or ddlSelectImEx.Text.Trim = "LogExpGenLOT" _
+            Or ddlSelectImEx.Text.Trim = "LogImpGenLOT" Or ddlSelectImEx.Text.Trim = "LogProductDetail" Or ddlSelectImEx.Text.Trim = "LogProductUnit" _
+            Or ddlSelectImEx.Text.Trim = "LogProvince" Or ddlSelectImEx.Text.Trim = "LogReferenceType" Or ddlSelectImEx.Text.Trim = "LogShipper" _
+            Or ddlSelectImEx.Text.Trim = "LogShippingMark" Or ddlSelectImEx.Text.Trim = "LogtransprotationMode" Or ddlSelectImEx.Text.Trim = "LogTruckWayBill" _
+            Or ddlSelectImEx.Text.Trim = "LogTruckWayBillDetail" Or ddlSelectImEx.Text.Trim = "LogWarehouseType" Or ddlSelectImEx.Text.Trim = "LogUser" _
+            Or ddlSelectImEx.Text.Trim = "LogTruckManifest" Or ddlSelectImEx.Text.Trim = "LogTruckManifestDetail" Or ddlSelectImEx.Text.Trim = "LogTruckOwner" _
+            Or ddlSelectImEx.Text.Trim = "LogTruckType" Then
+
+            If rdbByDate.Checked = True Then
+                If String.IsNullOrEmpty(txtdatepickerDate.Text.Trim) Then
+                    ScriptManager.RegisterStartupScript(Me, Me.GetType(), "alertMessage", "alert('กรุณาใส่ Date Start ก่อน');", True)
+                    Exit Sub
+                End If
+                If String.IsNullOrEmpty(txtdatepickerToDate.Text.Trim) Then
+                    ScriptManager.RegisterStartupScript(Me, Me.GetType(), "alertMessage", "alert('กรุณาใส่ Date From ก่อน');", True)
+                    Exit Sub
+                End If
+
+                CheckBy = "ByDate"
+
+                Session("CheckLog") = CheckLog
+                Session("CheckBy") = CheckBy
+                Session("formdate") = formdate
+                Session("toDate") = toDate
+                'Session("InvoiceNo") = InvoiceNo
+
+                Dim url As String = "ShowReport/ShowRptLog.aspx?Fromdate=" + formdate + "&Todate=" + toDate
+                ScriptManager.RegisterStartupScript(Me, Me.GetType(), "onclick", "javascript:window.open( '" + url + "','_blank','height=600px,width=1000px,scrollbars=1');", True)
+
+            ElseIf rdbByInvoice.Checked = True Then
+
+                ScriptManager.RegisterStartupScript(Me, Me.GetType(), "alertMessage", "alert('กรุณาใส่ Invoice No ก่อน');", True)
+                Exit Sub
+               
+            End If
+
+        End If
 
     End Sub
 
--- WMS/ShowReport/ShowRptLog.aspx.vb
+++ WMS/ShowReport/ShowRptLog.aspx.vb
@@ -1,8 +1,66 @@
-﻿Public Class ShowRptLog
+﻿Option Explicit On
+Option Strict On
+Option Infer On
+
+Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Shared
+Imports CrystalDecisions.Web
+Imports CrystalDecisions.ReportSource
+Imports CrystalDecisions.CrystalReports
+Imports System.Linq
+Imports Microsoft.Reporting.WebForms
+Imports System.Net
+Imports System.Globalization
+
+Public Class ShowRptLog
     Inherits System.Web.UI.Page
+    Private r As New ReportDocument
+    Dim path As String
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        If Not IsPostBack Then
+            Dim CheckLog As String = CStr(Session("CheckLog"))
+            Dim CheckBy As String = CStr(Session("CheckBy"))
+            Dim InvoiceNo As String = CStr(Session("InvoiceNo"))
+            Dim _cultureEnInfo As New Globalization.CultureInfo("en-US")
+            Dim formdate As String = CStr(Session("formdate"))
+            Dim toDate As String = CStr(Session("toDate"))
+            Try
+
+                If CheckLog = "LogInvoice" Or CheckLog = "LogInvoiceDetail" Or CheckLog = "LogPackingList" _
+            Or CheckLog = "LogImpInvoice" Or CheckLog = "LogImpInvoiceDetail" Or CheckLog = "LogImpPackingList" Then
+
+                    If CheckLog = "LogInvoice" Then
+                        r.Load(Server.MapPath("../Report/LogReport/RptLogInvoice.rpt"))
+                    ElseIf CheckLog = "LogInvoiceDetail" Then
+                        r.Load(Server.MapPath("../Report/LogReport/RptLogInvoiceDetail.rpt"))
+                    ElseIf CheckLog = "LogPackingList" Then
+                        r.Load(Server.MapPath("../Report/LogReport/RptLogPackingList.rpt"))
+                    ElseIf CheckLog = "LogImpInvoice" Then
+                        r.Load(Server.MapPath("../Report/LogReport/RptLogImpInvoice.rpt"))
+                    ElseIf CheckLog = "LogImpInvoiceDetail" Then
+                        r.Load(Server.MapPath("../Report/LogReport/RptLogImpInvoiceDetail.rpt"))
+                    ElseIf CheckLog = "LogImpPackingList" Then
+                        r.Load(Server.MapPath("../Report/LogReport/RptLogImpPackingList.rpt"))
+                    End If
+
+                    r.SetDatabaseLogon("LKBWarehouse", "7tFCca6pzt", "LKBWarehouse", "LKBWarehouse")
+
+                    If CheckBy = "ByDate" Then
+                        r.SetParameterValue("Date1", formdate)
+                        r.SetParameterValue("Date2", toDate)
+                        'r.SetParameterValue("Inv", txtInvoice.Text.Trim())
+                    Else
+                    End If
+
+
+                End If
+
+            Catch ex As Exception
+            End Try
+
+        End If
     End Sub
 
 End Class
